19274e9b11b1b7e26592c2cfdb7fc8850e56c105
AkarExcel <therealakarexcel@gmail>
2022-04-14 02:12:00 +0100

    My first commit

--- server.py
+++ server.py
@@ -2,6 +2,11 @@
 from os import environ
 from flask import Flask
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return"Home page"
+
 app.run(host= '0.0.0.0', port=environ.get('PORT'))
 
 
